e14c713a99ce2bf6e360543011fde7ec34de79d8
XING YAHAO <keidarcy.1015@gmail.com>
2020-12-09 11:50:48 +0900

    update

--- init.py
+++ init.py
@@ -6,6 +6,17 @@ for file in files:
   if not '.' in file:
     folders.append(file)
 
+links = {}
 for folder in folders:
-  readme_list = os.listdir('./' + folder) 
-  print(readme_list)
+  file_list = os.listdir('./' + folder) 
+  links[folder] = file_list
+
+print(links)
+
+print('--------------OO-----')
+
+
+with open("test.txt",'w',encoding = 'utf-8') as f:
+   f.write("my first file\n")
+   f.write("This file\n\n")
+   f.write("contains three lines\n")
