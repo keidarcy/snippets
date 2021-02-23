ef814f980efba0a88c09a26ee7044b618d28060e
XING YAHAO <keidarcy.1015@gmail.com>
2021-02-23 15:01:02 +0900

    regex parse

--- __tests__/regex/index.js
+++ __tests__/regex/index.js
@@ -1,11 +1,14 @@
-const parse = require('./parse');
+const parse = require('./parser');
+
 function stateMatchesStringAtIndex(state, str, i) {
   if (i >= str.length) {
     return [false, 0];
   }
+
   if (state.type === 'wildcard') {
     return [true, 1];
   }
+
   if (state.type === 'element') {
     const match = state.value === str[i];
     return [match, match ? 1 : 0];
@@ -17,17 +20,21 @@ function stateMatchesStringAtIndex(state, str, i) {
 
   throw new Error('Unsupported element type');
 }
+
 function test(states, str) {
   const queue = states.slice();
   let i = 0;
-  let currentState = queue.shift();
   const backtrackStack = [];
+  let currentState = queue.shift();
 
   const backtrack = () => {
     queue.unshift(currentState);
+
     let couldBacktrack = false;
+
     while (backtrackStack.length) {
       const { isBacktrackable, state, consumptions } = backtrackStack.pop();
+
       if (isBacktrackable) {
         if (consumptions.length === 0) {
           queue.unshift(state);
@@ -39,17 +46,25 @@ function test(states, str) {
         couldBacktrack = true;
         break;
       }
+
       queue.unshift(state);
       consumptions.forEach((n) => {
         i -= n;
       });
     }
+
+    if (couldBacktrack) {
+      currentState = queue.shift();
+    }
+
+    return couldBacktrack;
   };
 
   while (currentState) {
     switch (currentState.quantifier) {
       case 'exactlyOne': {
         const [isMatch, consumed] = stateMatchesStringAtIndex(currentState, str, i);
+
         if (!isMatch) {
           const indexBeforeBacktracking = i;
           const couldBacktrack = backtrack();
@@ -58,6 +73,7 @@ function test(states, str) {
           }
           continue;
         }
+
         backtrackStack.push({
           isBacktrackable: false,
           state: currentState,
@@ -67,8 +83,10 @@ function test(states, str) {
         currentState = queue.shift();
         continue;
       }
+
       case 'zeroOrOne': {
-        if (i > str.length) {
+        // steps++;
+        if (i >= str.length) {
           backtrackStack.push({
             isBacktrackable: false,
             state: currentState,
@@ -77,16 +95,18 @@ function test(states, str) {
           currentState = queue.shift();
           continue;
         }
+
         const [isMatch, consumed] = stateMatchesStringAtIndex(currentState, str, i);
+        i += consumed;
         backtrackStack.push({
           isBacktrackable: isMatch && consumed > 0,
           state: currentState,
           consumptions: [consumed]
         });
-        i += consumed;
         currentState = queue.shift();
         continue;
       }
+
       case 'zeroOrMore': {
         const backtrackState = {
           isBacktrackable: true,
@@ -95,32 +115,37 @@ function test(states, str) {
         };
 
         while (true) {
+          // steps++;
           if (i >= str.length) {
             if (backtrackState.consumptions.length === 0) {
-              backtrackState.consumptions.push(o);
+              backtrackState.consumptions.push(0);
               backtrackState.isBacktrackable = false;
             }
             backtrackStack.push(backtrackState);
             currentState = queue.shift();
             break;
           }
+
           const [isMatch, consumed] = stateMatchesStringAtIndex(currentState, str, i);
           if (!isMatch || consumed === 0) {
             if (backtrackState.consumptions.length === 0) {
-              backtrackState.consumptions.push(o);
+              backtrackState.consumptions.push(0);
               backtrackState.isBacktrackable = false;
             }
             backtrackStack.push(backtrackState);
             currentState = queue.shift();
             break;
           }
+
           backtrackState.consumptions.push(consumed);
           i += consumed;
         }
         continue;
       }
+
       default: {
-        throw new Error('Unsupported operation');
+        currentState;
+        throw new Error('Unsupported');
       }
     }
   }
@@ -130,6 +155,6 @@ function test(states, str) {
 
 const regex = 'a.*c';
 const states = parse(regex);
-const exampleStr = 'acc';
+const exampleStr = 'acsssssc';
 const result = test(states, exampleStr);
 console.log({ result });
