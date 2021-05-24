9c3ba29d921f8241cef87617a7aa263684ccae6f
XING YAHAO <keidarcy.1015@gmail.com>
2021-05-24 19:23:56 +0900

    update

--- __tests__/js/udp.js
+++
@@ -1,9 +0,0 @@
-const dgram = require('dgram');
-
-const socket = dgram.createSocket('udp4');
-
-socket.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-});
-
-socket.bind(8081);
